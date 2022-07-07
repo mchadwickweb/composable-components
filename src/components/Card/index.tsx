cbe287097b1984ba21ec30783222440e25dd6c06
Michael Chadwick <michael@interactstudio.co.uk>
2022-07-07 13:23:50 +0100

    add starting card markup

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 interface CardProps {
   title: string;
+  standfirst?: string;
+  media: {
+    src: string;
+    alt: string;
+  };
+  link: {
+    url: string;
+  };
 }
 
-export default function Card({ title }) {
+export default function Card({ title, standfirst, media, link }: CardProps) {
   return (
     <article>
+      <figure>
+        <img src={media.src} alt={media.alt} />
+      </figure>
       <h3>{title}</h3>
+      {standfirst ? <p>{standfirst}</p> : null}
+      <a href={link.url} tabIndex={0} aria-hidden="true">
+        {title}
+      </a>
     </article>
   );
 }
